bf6acdbd8fb006ecef6222a3d00dcd3da5fe87e4
Ezra <ezramosimann@gmail.com>
2022-06-22 10:14:36 +0200

    .

--- components/navbar.php
+++ components/navbar.php
@@ -3,7 +3,7 @@
 <nav>
     <ul>
         <!-- <li><a href=""><i class="fa-solid fa-house"></i></a></li> -->
-        <li class="transi-underline"><a href="../index.php">Home</a></li>
+        <li class="transi-underline"><a href="../index.html">Home</a></li>
         <li class="transi-underline"><a href="../pages/aboutme.php">About me</a></li>
         <li class="transi-underline"><a href="../pages/projects.php">Projects</a></li>
         <li class="transi-underline"><a href="../pages/contact.php">Contact</a></li>
