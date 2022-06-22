cf49205b925a0cb368dcdfab5658e6feb82a1f32
Ezra <ezramosimann@gmail.com>
2022-06-22 15:43:49 +0200

    nearly finished page 'projects', got to add phpmailer to 'contact'

--- pages/projects.php
+++ pages/projects.php
@@ -1,5 +1,3 @@
-<?php ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,9 +15,117 @@
     <link rel="stylesheet" href="../css/projects.css">
 </head>
 <body>
-<?php require '../components/navbar.php'?>
 
-<h2>W.I.P</h2>
+<?php require '../components/navbar.php' ?>
+<div id="projects">
+    <h2>My projects</h2>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project1">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 01</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project2">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 02</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project3">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 03</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project4">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 04</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project5">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 05</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project6">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 06</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project7">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 07</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+    <div class="project-content" id="project8">
+        <a target="_blank" href="#">
+            <img src="../images/Screenshot_pp.png" alt="img_project1">
+            <h3>EXAMPLE 08</h3>
+            <div class="expendable-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta enim error illo impedit incidunt
+                    laborum
+                    maxime minima minus nam optio quidem repellendus rerum, saepe, sit totam vel voluptas voluptatem
+                    voluptatum?</p>
+            </div>
+        </a>
+    </div>
+    <!-- --------------------------------------------------------------- -->
+</div>
+<script src="../js/functions.js"></script>
 </body>
 </html>
 
